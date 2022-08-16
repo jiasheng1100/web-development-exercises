86bcc7e03ac7279baf9c68370802b37f3726efc7
Jia Sheng <shengjia1100@gmail.com>
2022-08-16 17:34:19 +0200

    exercise 1.8&1.9 step2&step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,7 +10,29 @@ const Button = (props) => (
 
 const Count = ({ text, value }) => <div>{text} {value}</div>
 
-const Positive = ({ value }) => <div>positive {(value[0]/(value[0]+value[1]+value[2]))*100} %</div>
+const Positive = ({good, neutral, bad}) => <div>positive {(good/(good+neutral+bad))*100} %</div>
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good===0 && neutral===0 && bad===0){
+    return (
+      <div>
+        <Header header={"statistics"} />
+        No feedback given
+      </div>
+    )
+  }
+
+  return(  
+    <div>
+      <Header header={"statistics"} />
+      <Count text={"good"} value={good} />
+      <Count text={"neutral"} value={neutral} />
+      <Count text={"bad"} value={bad} />
+      <Count text={"average"} value={(good-bad)/(good+neutral+bad)} />
+      <Positive good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -24,13 +46,7 @@ const App = () => {
       <Button handleClick={() => setGood(good+1)} text="good" />
       <Button handleClick={() => setNeutral(neutral+1)} text="neutral" />
       <Button handleClick={() => setBad(bad+1)} text="bad" />
-      <Header header={"statistics"} />
-      <Count text={"good"} value={good} />
-      <Count text={"neutral"} value={neutral} />
-      <Count text={"bad"} value={bad} />
-      <Count text={"average"} value={(good-bad)/(good+neutral+bad)} />
-      <Positive value={[good, neutral, bad]} />
-      
+      <Statistics good={good} neutral={neutral} bad={bad} />     
     </div>
   )
 }
