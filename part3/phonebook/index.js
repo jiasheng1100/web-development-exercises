ecb75ab10a3450b75a0074f2ce996a96faaa29b3
Jia Sheng <shengjia1100@gmail.com>
2022-10-03 13:08:01 +0200

    ex.3.3 phonebook backend step3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -32,6 +32,17 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people \n ${new Date()}` )
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)  
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
