90010371616b8c5f540024e3d7bce9e1a9598e4c
Jia Sheng <shengjia1100@gmail.com>
2022-10-03 12:41:14 +0200

    ex.3.2 phonebook backend step2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -24,12 +24,12 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people \n ${new Date()}` )
 })
 
 const PORT = 3001
